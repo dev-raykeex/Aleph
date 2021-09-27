ef7ed16be17648b0883d39690dfa985751eccfe5
Whiteline <48377708+xwhiteex@users.noreply.github.com>
2021-09-27 16:30:03 +0300

    whiteex
    - added the "AllowDebug" bool in code

--- Source/Aleph/PCharacter.h
+++ Source/Aleph/PCharacter.h
@@ -18,6 +18,8 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Debug")
+		bool AllowDebug = false;
 
 protected:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Movement")
