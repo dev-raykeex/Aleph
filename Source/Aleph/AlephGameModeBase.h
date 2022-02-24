d17c31046c4a61f52a28cfb9baff513d7b169241
Morar Andrei <whiteex@icloud.com>
2022-02-24 11:48:16 +0200

    Upload UE5 project files.

---
+++ Source/Aleph/AlephGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AlephGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALEPH_API AAlephGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
